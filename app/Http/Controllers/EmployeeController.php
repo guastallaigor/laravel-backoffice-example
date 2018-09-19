f7947c70b83204b119f2769240182102ae698398
Igor Guastalla de Lima <limaguastallaigor@gmail.com>
2018-09-19 00:35:08 -0300

    Changes 2

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -16,26 +16,32 @@ class EmployeeController extends Controller
 
         return response()->json($data);
     }
+
     public function store(EmployeeRequest $request)
     {
         return $this->save($request, new Employee());
     }
+
     public function show(Employee $employee)
     {
         return response()->json($employee, $employee->wasRecentlyCreated ? 201 : 200);
     }
+
     public function update(EmployeeRequest $request, Employee $employee)
     {
         return $this->save($request, $employee);
     }
+
     private function save(EmployeeRequest $request, Employee $employee)
     {
         $employee->nome = $request->json('nome');
         $employee->email = $request->json('email');
         $employee->observacao = $request->json('observacao');
         $employee->save();
+
         return $this->show($employee);
     }
+
     public function destroy(Employee $employee)
     {
         $employee->delete();
