c4e200db521db02a44129c813d7a28571cec7159
Igor Guastalla de Lima <igor.lima@sabium.com.br>
2018-10-04 07:49:47 -0300

    Changed folders and scrutinizer. 2

--- config/auth.php
+++ config/auth.php
@@ -67,7 +67,7 @@ return [
     'providers' => [
         'users' => [
             'driver' => 'eloquent',
-            'model' => App\Employee::class,
+            'model' => App\Models\Employee::class,
         ],
 
         // 'users' => [
